bbb15201378203786351701b99db64ace23ac610
Jonathan Gin <jonathangin52@github.com>
2022-08-02 16:38:05 -0400

    Update readme

--- connect4/markdown_generator.rb
+++ connect4/markdown_generator.rb
@@ -25,9 +25,9 @@ class MarkdownGenerator
 
         [![Twitter Badge](https://img.shields.io/badge/-@JonathanGin52-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/jonathangin52)](https://twitter.com/jonathangin52) [![Linkedin Badge](https://img.shields.io/badge/-JonathanGin-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/jonathangin/)](https://www.linkedin.com/in/jonathangin/)
 
-        Nice to meet you! I'm currently studying and working as a [Dev Degree](https://devdegree.ca/) intern [@Shopify](https://www.shopify.com/).
-        I've previously worked on building the [Shopify Fulfillment Network](https://www.shopify.com/fulfillment) as a fullstack developer, Shopify's Experimentation Platform as a data developer, and Shopify Checkout as a backend developer.
-        Nowadays, I'm working on Shopify Caching Platform as a production engineer.
+        Nice to meet you! I'm currently working at GitHub as a software engineer.
+        I recently graduated from the [Shopify Dev Degree](https://devdegree.ca/) program in Spring 2022.
+        I've previously worked on building the [Shopify Fulfillment Network](https://www.shopify.com/fulfillment) as a fullstack developer, Shopify's Experimentation Platform as a data developer, Shopify Checkout as a backend developer, and Shopify's Caching Platform as a production engineer.
 
         ## :game_die: Join my community Connect Four game!
         ![](https://img.shields.io/badge/Moves%20played-#{total_moves_played}-blue)
