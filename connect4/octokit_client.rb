98889680e8468515146bf7364d0ea6c65fc85d07
Jonathan Gin <jonathangin52@github.com>
2022-08-02 16:48:52 -0400

    Update client

--- connect4/octokit_client.rb
+++ connect4/octokit_client.rb
@@ -2,14 +2,14 @@ require 'octokit'
 
 class OctokitClient
   PREVIEW_HEADERS = [
-    ::Octokit::Preview::PREVIEW_TYPES[:reactions],
-    ::Octokit::Preview::PREVIEW_TYPES[:integrations]
+    Octokit::Preview::PREVIEW_TYPES[:reactions],
+    Octokit::Preview::PREVIEW_TYPES[:integrations]
   ].freeze
 
   def initialize(github_token:, repository:, issue_number:)
     @octokit = Octokit::Client.new(access_token: github_token)
     @octokit.auto_paginate = true
-    @octokit.default_media_type = ::Octokit::Preview::PREVIEW_TYPES[:integrations]
+    @octokit.default_media_type = Octokit::Preview::PREVIEW_TYPES[:integrations]
     @repository = repository
     @issue_number = issue_number
   end
